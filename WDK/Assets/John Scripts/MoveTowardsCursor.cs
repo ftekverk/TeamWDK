1b16bf9e9198bcc9a1ef5363271e7dc69584f8f0
soul2598 <93749464+soul2598@users.noreply.github.com>
2021-11-26 15:47:29 -0600

    Bullets travel to the cursor at a consistent speed now
    Turns out I'm dumb and just needed to not use physics for it.

--- WDK/Assets/John Scripts/MoveTowardsCursor.cs
+++ WDK/Assets/John Scripts/MoveTowardsCursor.cs
@@ -4,31 +4,15 @@ using UnityEngine;
 
 public class MoveTowardsCursor : MonoBehaviour
 {
-    Rigidbody2D bulletRb;
-    Vector3 mousePos;
-    Vector3 mousePosWS;
-    Vector3 bulletPos;
-    Vector3 targetPos;
-
-    float yChange;
-    float xChange;
+    [SerializeField] float speed;
 
     void Start()
     {
-        bulletRb = GetComponent<Rigidbody2D>();
-        bulletPos = transform.position;
-        mousePos = Input.mousePosition;
-        mousePosWS = Camera.main.ScreenToWorldPoint(mousePos);
-        targetPos = new Vector3(mousePosWS.x, mousePosWS.y, 0);
-
-        yChange = mousePosWS.y - bulletPos.y;
-        xChange = mousePosWS.x - bulletPos.x;
-
-        bulletRb.velocity = new Vector2(xChange, yChange);
+       
     }
 
     void Update()
     {
-        
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
     }
 }

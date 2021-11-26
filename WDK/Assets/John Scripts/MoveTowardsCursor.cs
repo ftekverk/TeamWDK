c8e4d581aac9bbe5deba790322fc0c3ea43fc101
soul2598 <93749464+soul2598@users.noreply.github.com>
2021-11-26 09:36:23 -0600

    Bullets now move towards cursor
    The bullets gain a velocity equal to the slope of the target trajectory. They currently travel at different speeds based on how far the cursor is from the player.

--- WDK/Assets/John Scripts/MoveTowardsCursor.cs
+++ WDK/Assets/John Scripts/MoveTowardsCursor.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class MoveTowardsCursor : MonoBehaviour
 {
+    Rigidbody2D bulletRb;
     Vector3 mousePos;
     Vector3 mousePosWS;
     Vector3 bulletPos;
     Vector3 targetPos;
 
+    float yChange;
+    float xChange;
+
     void Start()
     {
+        bulletRb = GetComponent<Rigidbody2D>();
         bulletPos = transform.position;
         mousePos = Input.mousePosition;
         mousePosWS = Camera.main.ScreenToWorldPoint(mousePos);
         targetPos = new Vector3(mousePosWS.x, mousePosWS.y, 0);
+
+        yChange = mousePosWS.y - bulletPos.y;
+        xChange = mousePosWS.x - bulletPos.x;
+
+        bulletRb.velocity = new Vector2(xChange, yChange);
     }
 
     void Update()
     {
-        transform.position = targetPos;
+        
     }
 }

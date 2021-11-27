0301721844b759137634200cfa2c2a5420908f4c
soul2598 <93749464+soul2598@users.noreply.github.com>
2021-11-27 07:51:43 -0600

    Added folders

---
+++ WDK/Assets/John Scripts/Player Controller Scripts/PlayerStates.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public bool jumping;
+    public bool recoiling;
+}

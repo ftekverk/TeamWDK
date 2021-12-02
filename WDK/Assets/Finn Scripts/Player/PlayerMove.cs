5271307546d7f3bee18509bcf96ab91155b89b09
ftekverk <86449762+ftekverk@users.noreply.github.com>
2021-12-01 21:39:36 -0500

    edits

--- WDK/Assets/Finn Scripts/Player/PlayerMove.cs
+++ WDK/Assets/Finn Scripts/Player/PlayerMove.cs
@@ -8,12 +8,14 @@ public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rb;
     SpriteRenderer spriteRenderer;
-    public float runSpeed = 10f;
+    public float runSpeed = 8f;
     private float hMove;
     public PlayerJump jumpscript;
 
-    KeyCode rightMove = KeyCode.RightArrow;
-    KeyCode leftMove = KeyCode.LeftArrow;
+    //KeyCode rightMove = KeyCode.RightArrow;
+    //KeyCode leftMove = KeyCode.LeftArrow;
+
+    public float hInput;
 
     // Start is called before the first frame update
     void Start()
@@ -25,18 +27,15 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        hInput = Input.GetAxis("Horizontal");
         //if we're grounded we can move horizontally
-        if(jumpscript.grounded){
-          if(Input.GetKey(rightMove)){
-              rb.velocity = new Vector2(runSpeed, rb.velocity.y);
-              spriteRenderer.flipX = true;
-          }
-          if(Input.GetKey(leftMove)){
-              rb.velocity = new Vector2(-runSpeed, rb.velocity.y);
-              spriteRenderer.flipX = false;
-          }
+        if (jumpscript.grounded){
+            rb.velocity = new Vector2(hInput * runSpeed, rb.velocity.y);
+            if (hInput > 0) spriteRenderer.flipX = true;
+            else if (hInput < 0) spriteRenderer.flipX = false;
         }
-
-
     }
+
+
+   
 }

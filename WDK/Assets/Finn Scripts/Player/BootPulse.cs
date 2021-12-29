51b0c38933aa535dc4bbf2ad6f1f431cbff67923
ftekverk <86449762+ftekverk@users.noreply.github.com>
2021-12-29 01:13:37 -0500

    Update BootPulse.cs

--- WDK/Assets/Finn Scripts/Player/BootPulse.cs
+++ WDK/Assets/Finn Scripts/Player/BootPulse.cs
@@ -21,18 +21,16 @@ public class BootPulse : MonoBehaviour
 
     void Update()
     {
+        //Our vector is from our player to their feet
+        pulseDirection = feet.position - transform.position;
+
+        //store position of feet as a 2D vector
+        feet2Dposition = new Vector2(feet.position.x, feet.position.y);
+
+        //the spot to spawn our pulse is at our feet plus a fraction of our direction vector
+        pulseSpawnLocation = feet2Dposition + (0.5f * pulseDirection);
         //if we are doublejumping and not grounded
-        if(jumpscript.doubleJumpCalled && !jumpscript.grounded){
-          //Our vector is from our player to their feet
-          pulseDirection = feet.position - transform.position;
-
-          //store position of feet as a 2D vector
-          feet2Dposition = new Vector2(feet.position.x, feet.position.y);
-
-            //the spot to spawn our pulse is at our feet plus a fraction of our direction vector
-          pulseSpawnLocation = feet2Dposition + (0.5f * pulseDirection);
-
-
+        if (jumpscript.doubleJumpCalled && !jumpscript.grounded){
           pulse = Instantiate(bootPulsePrefab, pulseSpawnLocation, transform.rotation);
           pulse.GetComponent<Rigidbody2D>().velocity = pulseDirection * pulseSpeed;
         }

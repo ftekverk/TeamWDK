099d71c1eccff4d1ec20bf3bb86a816c610fac14
ftekverk <86449762+ftekverk@users.noreply.github.com>
2021-12-29 04:11:29 -0500

    linecast efforts

--- WDK/Assets/Finn Scripts/Player/BootsLineCast.cs
+++ WDK/Assets/Finn Scripts/Player/BootsLineCast.cs
@@ -4,34 +4,44 @@ using UnityEngine;
 
 public class BootsLineCast : MonoBehaviour
 {
+    private LineRenderer lr;
+    private Transform[] points;
+
+
     public BootPulse pulseScript;
     public PlayerJump jumpScript;
+    public Vector2 pulseDirection;
 
-    private Vector2 pulseDirection;
-    // Start is called before the first frame update
+    public LayerMask layersToIgnore;
+
+    public float range = 100f;
     void Start()
     {
-            
+        lr = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // pulseDirection = pulseScript.pulseDirection;
-
+        
         if (!jumpScript.grounded) //if not grounded, draw a line to the next collider
         {
-            Debug.Log(pulseScript.pulseSpawnLocation);
-           Debug.DrawRay(pulseScript.pulseSpawnLocation, pulseScript.pulseDirection * 10, Color.yellow);
-            if (Physics.Raycast(pulseScript.pulseSpawnLocation, pulseDirection))
+            pulseDirection = pulseScript.pulseDirection;
+            
+             //RaycastHit2D hit = Physics2D.Raycast(pulseScript.pulseSpawnLocation, pulseDirection * 10, range, layersToIgnore);
+            if (Physics2D.Raycast(pulseScript.pulseSpawnLocation, pulseDirection, range, layersToIgnore))
             {
-                Debug.Log("yay");
+                Debug.DrawRay(pulseScript.pulseSpawnLocation, pulseDirection * 10);
+                //this.transform.position = pulseScript.pulseSpawnLocation;
+                Debug.Log("Here1");
+              //  lr.SetPosition(1, pulseScript.pulseSpawnLocation);
+               // lr.SetPosition(2, hit.point);
+                Debug.Log("Here2");
+
             }
+    
         }
 
     }
-
-
-
 
 }

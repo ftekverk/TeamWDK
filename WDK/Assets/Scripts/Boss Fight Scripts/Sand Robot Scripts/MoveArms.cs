f92e47451398dc278380541c444028266b000b1f
ftekverk <86449762+ftekverk@users.noreply.github.com>
2022-01-03 21:42:23 -0500

    robot arms moving
    next i'll set bounds and make random

--- WDK/Assets/Scripts/Boss Fight Scripts/Sand Robot Scripts/MoveArms.cs
+++ WDK/Assets/Scripts/Boss Fight Scripts/Sand Robot Scripts/MoveArms.cs
@@ -6,20 +6,34 @@ public class MoveArms : MonoBehaviour
 {
     public GameObject rightArm;
     public GameObject leftArm;
+    public Rigidbody2D rightArmRb;
+    public Rigidbody2D leftArmRb;
+    public Vector2 forceVector;
+
+    public float horizontalArmMoveSpeed = 0.001f;
+    public float verticalArmMoveSpeed = 0.001f;
+    public float maxVerticalSpeed = 3f;
+    public float maxHorizontalSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
-
+        rightArmRb = rightArm.GetComponent<Rigidbody2D>();
+        leftArmRb = leftArm.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        moveArmsDown();
     }
 
     public void moveArmsDown()
     {
-
+        forceVector = new Vector2(0f, -verticalArmMoveSpeed);
+        if(-rightArmRb.velocity.y < maxVerticalSpeed)
+        {
+            rightArmRb.velocity += forceVector;
+            leftArmRb.velocity += forceVector;
+        }
     }
 }

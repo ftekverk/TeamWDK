9df6ef2d539e95b9354ba893bca3fc702810ea59
soul2598 <93749464+soul2598@users.noreply.github.com>
2022-01-09 09:54:02 -0600

    Added detailed comments to my camera script
    If it still doesn't make sense please let me know

--- WDK/Assets/Scripts/Camera/FollowWithinBounds.cs
+++ WDK/Assets/Scripts/Camera/FollowWithinBounds.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class FollowWithinBounds : MonoBehaviour
 {
-    //public GameObject player;
+    //This script allows the camera to follow the player.
+    //The camera won't move up with the player until their y value is = shiftValue.
+
+
     public Transform player;
 
-    public Vector3 minValues;
-    public Vector3 maxValues;
+    public Vector3 minValues1;
+    public Vector3 maxValues1;
 
     public Vector3 minValues2;
     public Vector3 maxValues2;
@@ -20,15 +23,6 @@ public class FollowWithinBounds : MonoBehaviour
 
     public float shiftValue;
 
-    //=======================================================================================================================================
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Vector3 targetPosition = player.position + offset;
@@ -40,16 +34,12 @@ public class FollowWithinBounds : MonoBehaviour
         }
         else
         {
-            cameraBounds = getCameraBounds(targetPosition, minValues, maxValues);
+            cameraBounds = getCameraBounds(targetPosition, minValues1, maxValues1);
         }
 
 
         transform.position = cameraBounds;
-
-        //transform.position = player.transform.position + offset;
     }
-
-    //=======================================================================================================================================
 
     Vector3 getCameraBounds(Vector3 targetPositionF, Vector3 minValuesF, Vector3 maxValuesF)
     {  //Function that recieves a targetposition and values for the bounds, then returns the clamped bounds

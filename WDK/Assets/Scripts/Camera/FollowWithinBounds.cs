af269bb392f86f4db0cf60e037466c59c7235d96
soul2598 <93749464+soul2598@users.noreply.github.com>
2022-01-09 10:07:02 -0600

    minor changes

--- WDK/Assets/Scripts/Camera/FollowWithinBounds.cs
+++ WDK/Assets/Scripts/Camera/FollowWithinBounds.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class FollowWithinBounds : MonoBehaviour
 {
-    //This script allows the camera to follow the player.
-    //The camera won't move up with the player until their y value is = shiftValue.
+    // This script allows the camera to follow the player.
+    // The camera won't move up with the player until their y value is = shiftValue.
 
+    // If necessary this script could be expanded to have more sets of min/max values for different shift values.
 
     public Transform player;
 
-    public Vector3 minValues1;
+    public Vector3 minValues1; // These values are the min and max positions for the camera before the player has reached the shiftValue.
     public Vector3 maxValues1;
 
-    public Vector3 minValues2;
+    public Vector3 minValues2; // These values are the min and max positions for the camera after the player has reached the shiftValue.
     public Vector3 maxValues2;
 
-    public Vector3 offset;
-    public Vector3 offset2;
+    public Vector3 offset; // Camera offset from the player position before shiftValue.
+    public Vector3 offset2; // Camera offset from the player position after shiftValue.
 
-    private Vector3 cameraBounds;
+    private Vector3 cameraBounds; // Holds the values for the camera position after they are clamped.
 
-    public float shiftValue;
+    public float shiftValue; // The y value the player's position has to reach for the camera to shift.
 
     void Update()
     {
@@ -42,7 +43,8 @@ public class FollowWithinBounds : MonoBehaviour
     }
 
     Vector3 getCameraBounds(Vector3 targetPositionF, Vector3 minValuesF, Vector3 maxValuesF)
-    {  //Function that recieves a targetposition and values for the bounds, then returns the clamped bounds
+    {  // This function recieves the target position for the camera, and makes sure it is within the min and max allowed values
+       // It then returns the clamped values for the camera to move to
         Vector3 cameraBoundsF;
 
         cameraBoundsF = new Vector3(
